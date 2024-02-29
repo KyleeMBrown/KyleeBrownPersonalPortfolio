efe72742278e684c5d7ec19da729d8c604f30b90
Kylee <kyleebrown.work@gmail.com>
2024-02-29 01:34:16 -0600

    finished layout started homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import {NavHashLink} from "react-router-hash-link";
 import { motion } from "framer-motion"
 
+
 const NavBar = () => {
 
     return(
         <>
-        <motion.nav animate={{opacity:[0,1]}} transition={{duration:10}} class="flex p-4 pt-8 justify-between ">
-            <ul class=" justify-between w-[60%] flex text-white text-[15px] ">
-                <li class=" font-curly text-[23px] text-center font-light text-pink-300">Portfolio</li>
-            <NavHashLink smooth to="/" ><li>Home</li></NavHashLink>  
+        <motion.nav animate={{opacity:[0,1]}} transition={{duration:7, delay:0.5}} class=" flex items-center p-4 pt-8 justify-between ">
+            <ul class=" justify-between items-center w-[60%] flex text-white text-[15px] ">
+                <motion.li whileHover={{ scale:1.05}} transition={{type:"spring"}} class=" font-curly text-[20px] text-center font-light bg-gradient-to-r from-pink-800 to-pink-700 p-[12px] rounded-[28px] shadow-lg ">KB Portfolio</motion.li>
+            <NavHashLink class="active:text-gray-600" smooth to="/" ><li>Home</li></NavHashLink>  
               <NavHashLink smooth to="/About" ><li>About</li></NavHashLink>  
               <NavHashLink smooth to="/Education" ><li>Education</li></NavHashLink>  
-              <NavHashLink smooth to="/#About" ><li>Projects</li></NavHashLink>
-              <NavHashLink smooth to="/#About" ><li>Skills</li></NavHashLink>
+              <NavHashLink smooth to="/Projects" ><li>Projects</li></NavHashLink>
+              <NavHashLink smooth to="/Skills" ><li>Skills</li></NavHashLink>
             </ul>
-            <div class="flex">
-                <h1 class="text-white pr-5">Visitor</h1>
-                <img src="" alt="" class="bg-white w-10 h-10"/>
+            <div class="flex items-center">
+                <h1 class="text-white pr-6">Profile</h1>
+                <motion.img whileHover={{scale:1.05, rotate:90}} src="public/Images/Tom and Jerry.jpeg" alt="" class="bg-white w-[65px] hover:cursor-pointer h-[65px] rounded-[50px] "/>
             </div>
            
         </motion.nav>
--- src/components/Sections/Homepage.jsx
+++ src/components/Sections/Homepage.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { useRef } from 'react';
+import {motion} from 'framer-motion'
 
 function Homepage() {
+const carousel = useRef()
+
+    const handleLeft = () => {
+        if(window.innerWidth > 500){
+          carousel.current.scrollLeft -= 800
+        }else{
+          carousel.current.scrollLeft -=  800
+        }
+       
+      }
+
+      const handleRight = () => {
+        if(window.innerWidth > 500){
+          carousel.current.scrollLeft += 800
+        }else{
+          carousel.current.scrollLeft += 900
+        }
+      }
+
   return (
-    <div class= "w-full"> Home</div>
+<>
+    <motion.div animate={{opacity:[0,1]}} transition={{duration:6, delay:2}} class= "w-full flex justify-center h-[85.5%]" > 
+    <div onClick={handleLeft} class="flex rotate-180 items-center justify-center  h-[48%] mt-1 bg-gradient-to-r from-black hover:to-black hover:cursor-pointer rounded-bl-[20px]  rounded-tl-[20px] w-[3%]">
+            <h1  class="text-[25px] text-white text-center rotate-90"> ^ </h1>
+        </div>
+    <div ref={carousel} class="w-[97%] scroll-smooth  overflow-x-auto overflow-y-visible p-0 whitespace-nowrap h-[48%] no-scrollbar snap-x snap-mandatory  ">
+        <div class="w-[98%] snap-center bg mr-3 inline-block mt-4 h-[93%] rounded-[18px] shadow-[inset_0_0_15px_5px_#000000]"></div>
+        <div class="w-[98%] snap-center inline-block mr-3 mt-4 h-[93%] rounded-[18px] shadow-[inset_0_0_15px_1px_#000000]"></div>
+        <div class="w-[98%] snap-center bg mr-3 inline-block mt-4 h-[93%] rounded-[18px] shadow-[inset_0_0_15px_5px_#000000]"></div>
+        <div class="w-[98%] snap-center inline-block mr-3 mt-4 h-[93%] rounded-[18px] shadow-[inset_0_0_15px_1px_#000000]"></div>
+        
+
+        </div>
+        <div onClick={handleRight} class="flex items-center justify-center  h-[48%] mt-1 bg-gradient-to-r from-black hover:to-black hover:cursor-pointer rounded-bl-[20px]  rounded-tl-[20px] w-[3%]">
+            <h1  class="text-[25px] text-white text-center rotate-90"> ^ </h1>
+        </div>
+        
+    </motion.div>
+</>
+    
   )
 }
 
