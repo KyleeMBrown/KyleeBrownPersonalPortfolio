b1401dea92309c3796f9c3c463d3edb93234e89a
Kylee <kyleebrown.work@gmail.com>
2024-03-10 00:20:09 -0600

    soft launch

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./components/Sections/About";
 import Education from './components/Sections/Education';
 import Homepage from './components/Sections/Homepage';
 import Projects from './components/Sections/Projects';
-
+import Skills from './components/Sections/Skills';
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
       <Route path="/About" element={<About/>}></Route>
       <Route path="/Education" element={<Education/>}></Route>
       <Route path="/Projects" element={<Projects/>}></Route>
+      <Route path="/Skills" element={<Skills/>}></Route>
     </Routes>
    
 </BrowserRouter>
---
+++ src/components/Sections/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <div class="w-full h-[70.5%] flex items-center justify-center text-[67px] ">
+        <h1 class="rotate-[-12deg] tracking-[18px] bg-white p-3">UNDER CONSTRUCTION</h1>
+        <h1 class="absolute rotate-[12deg] tracking-[18px] bg-white p-3 drop-shadow-xl">UNDER CONSTRUCTION</h1>
+    </div>
+  )
+}
+
+export default Skills
