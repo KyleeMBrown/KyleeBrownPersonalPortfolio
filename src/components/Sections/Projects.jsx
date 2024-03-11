66d5a01e5e61972fcb7f3bd682991367b6e2bbf8
Kylee <kyleebrown.work@gmail.com>
2024-03-11 16:33:52 -0500

    added mem game

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -169,7 +169,7 @@ function Projects() {
                   <p class="indent-3 mt-1">
                   I built this project in order to strengthen my javascript data structures and algorithms as well as iterations!
                   I combined my current two favorite things for this project: coding & Adventure Time!<br />
-                      
+                  <a href="https://themed-mem-game.netlify.app/" target='_blank' class="text-blue-200 underline">Link to Website Here</a>
                      
                   </p>
                 
