b2722f3a31f3d14cc50bcc71c39875c41af9b476
Kylee <kyleebrown.work@gmail.com>
2024-03-11 00:54:49 -0500

    fixed homepage

--- src/components/Sections/Homepage.jsx
+++ src/components/Sections/Homepage.jsx
@@ -28,13 +28,13 @@ const carousel = useRef()
 
   return (
 <>
-<div class="w-full h-[70.5%]">
-<motion.div animate={{opacity:[0,1]}} transition={{duration:3, delay:1, repeat:0}} class= "w-full flex justify-center h-[85.5%]" > 
-    <div onClick={handleLeft} class="flex rotate-180 items-center justify-center  h-[48%] mt-1 bg-gradient-to-r from-black hover:to-black hover:cursor-pointer rounded-bl-[20px]  rounded-tl-[20px] w-[3%]">
+<div class="w-full h-full bg-pink-">
+<motion.div animate={{opacity:[0,1]}} transition={{duration:3, delay:1, repeat:0}} class= "w-full flex justify-center h-[300px] bg-yellow-" > 
+    <div onClick={handleLeft} class="flex rotate-180 items-center justify-center  h-[100%] bg-gradient-to-r from-black hover:to-black hover:cursor-pointer rounded-bl-[20px]  rounded-tl-[20px] w-[3%]">
             <h1  class="text-[25px] text-white text-center rotate-90"> ^ </h1>
         </div>
 
-    <div ref={carousel} class="w-[97%] scroll-smooth  overflow-x-auto overflow-y-hidden p-0 whitespace-nowrap h-[48%] no-scrollbar snap-x snap-mandatory  ">
+    <div ref={carousel} class="w-[97%]  scroll-smooth  overflow-x-auto overflow-y-hidden p-0 whitespace-nowrap h-[100%] no-scrollbar snap-x snap-mandatory  ">
        
         <div class="w-[98%] snap-center inline-block mr-3 mt-4 h-[93%] bg-[url('/Images/WTMWBanner.png')] bg-contain bg-[#FFEAF7] bg-no-repeat bg-center rounded-[18px] shadow-[inset_0_0_0px_1px_#000000]"></div>
         <div class="w-[98%] bg-[url('/Images/RecommendedMovie/BigHero6.jpeg')] bg-contain  bg-center bg-red-400 snap-center mr-3 inline-block mt-4 h-[93%] rounded-[18px] shadow-[inset_0_0_15px_5px_#000000]">
@@ -50,12 +50,12 @@ const carousel = useRef()
         
         
     </div>
-        <div onClick={handleRight} class="flex items-center justify-center  h-[48%] mt-1 bg-gradient-to-r from-black hover:to-black hover:cursor-pointer rounded-bl-[20px]  rounded-tl-[20px] w-[3%]">
+        <div onClick={handleRight} class="flex items-center justify-center h-[100%]    bg-gradient-to-r from-black hover:to-black hover:cursor-pointer rounded-bl-[20px]  rounded-tl-[20px] w-[3%]">
             <h1  class="text-[25px] text-white text-center rotate-90"> ^ </h1>
         </div>
     </motion.div>
-    <div class=" absolute  w-full  bottom-[33%]">
-      <div class=" justify-center text-white flex gap-6">
+    <div class="h-[30%] flex items-center justify-center bg-blue-  w-full ">
+      <div class="flex justify-center text-white items-center  gap-6">
      <a href="/Resume Kylee Brown (28).pdf" target='_blank'><motion.div whileHover={{ scale:1.08, borderColor: 'white'}} transition={{duration:0.4, type:"ease"}} class=" hover:cursor-pointer w-[255px] hover:border-[5px] rounded-[20px] h-[160px]  flex items-center justify-center bg-pink-800 hover:bg-pink-200 hover:text-pink-800 transition-colors drop-shadow-2xl">
           <h1 class="  text-[25px] ">RESUME</h1>
         </motion.div></a>
@@ -76,7 +76,7 @@ const carousel = useRef()
         </motion.div></NavHashLink>
       </div>
     </div>
-    <div class="flex items-center h-[41%] mt-[-42px]  ">
+    <div class="flex items-center h-[228px] pb-2 mt-[13px] bg-  ">
       <CarouselComponent>
         <a href="https://www.linkedin.com/in/kylee-brown-7167b9274/" target='_blank' ><div class="h-full w-[28%] snap-center bg-[url('/Images/default-share.png')] bg-cover shadow-2xl hover:cursor-pointer hover:border-[3px] border-blue-400 bg-center mr-2 ml-2 inline-block rounded-[15px] "></div></a>
         <a href="https://snhu.joinhandshake.com/stu/users/44118380" target='_blank'><div class="h-full w-[28%] snap-center bg-[url('/Images/handshakeLogo.webp')] bg-cover shadow-2xl hover:cursor-pointer hover:border-[3px]  bg-center mr-2 ml-2 inline-block rounded-[15px] "></div></a>
