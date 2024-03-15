3162fbc102e6a8ca5b35f6720ab9fdad73284d89
Kylee <kyleebrown.work@gmail.com>
2024-03-14 23:30:24 -0500

    skills page finished

--- src/components/Sections/Skills.jsx
+++ src/components/Sections/Skills.jsx
@@ -2,9 +2,72 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div class="w-full h-[70.5%] flex items-center justify-center text-[67px] ">
-        <h1 class="rotate-[-12deg] tracking-[18px] bg-white p-3">UNDER CONSTRUCTION</h1>
-        <h1 class="absolute rotate-[12deg] tracking-[18px] bg-white p-3 drop-shadow-xl">UNDER CONSTRUCTION</h1>
+    <div class="w-full h-[84.5%] flex justify-center items-center">
+      <div class='flex justify-center items-center shadow-[inset_0_0_15px_1px_black]'>
+        <div class='grid gap-x-20 gap-y-10 grid-cols-5 p-10'>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-javascript-48.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">JS</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-react-24.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">REACT</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-framer-motion.webp" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white pt-1">Framer Motion</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-css-48.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">CSS</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-tailwind-css-48.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Tailwind CSS</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-python-94 (1).png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Python</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/Scikit-learn.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Scikit-Learn</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-numpy-48.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Numpy</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-Matplotlib-logo.svg.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Matplot</h3>
+          </div>
+
+          <div class="w-[90px] ">
+            <img src="/Icons/icons8-pandas-48.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Numpy</h3>
+          </div>
+
+          
+
+          <div class="w-[90px] ">
+            <img src="/Icons/Git_icon.svg.png" alt="CSS" class="w-[90px]" />
+            <h3 class="text-center text-white">Git Version Control</h3>
+          </div>
+
+        </div>
+        
+
+      </div>
+       
     </div>
   )
 }
